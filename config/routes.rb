dd2e299fbb31ad9ac07cca31ffdbfc5b32aa1211
nwalker2398 <nwalker2398@yahoo.com>
2023-12-04 13:53:13 -0500

    Updated routes, controller path, test files, and README to use versioning in the url.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
+  namespace :v1 do
+    get "up" => "rails/health#show", as: :rails_health_check
 
-  get '/records/:bib_id', to: 'records#info_json'
-  get '/records/:bib_id/record.marc', to: 'records#bib_record_marc'
-  get '/records/:bib_id/holdings/:holdings_record_id/record.marc', to: 'records#holdings_record_marc'
+    get '/records/:bib_id', to: 'records#info_json'
+    get '/records/:bib_id/record.marc', to: 'records#bib_record_marc'
+    get '/records/:bib_id/holdings/:holdings_record_id/record.marc', to: 'records#holdings_record_marc'
+  end
 end
